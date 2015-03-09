046ed8c0bd1f96db765e7fa26b7bd6130a787556
Fraser Scott-Morrison <fraserscottmorrison@me.com>
2015-03-09 17:09:21 +1300

    Fix for keyboard changing height

--- Classes/IHKeyboardAvoiding.m
+++ Classes/IHKeyboardAvoiding.m
@@ -66,7 +66,7 @@ static float _minimumAnimationDuration;
             //showing and docked
             if (triggerView) {
                 float diff = 0;
-                if (keyboardHeightDiff > 0) {
+                if (keyboardHeightDiff != 0) {
                     diff = keyboardHeightDiff;
                 }
                 else {
@@ -113,7 +113,7 @@ static float _minimumAnimationDuration;
                         default:
                         {
                             float minimumDisplacement = fmaxf(displacement, diff);
-                            displacement = minimumDisplacement - (keyboardHeightDiff <= 0 ? _paddingCurrent : 0);
+                            displacement = minimumDisplacement - (keyboardHeightDiff == 0 ? _paddingCurrent : 0);
                             break;
                         }
                     }
