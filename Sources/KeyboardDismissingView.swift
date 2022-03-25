7b1abf5ad63c76e0195bf15121120f170a772e17
Khan <mushrankhan76@gmail.com>
2022-03-25 11:12:37 +0530

    throwing error while archiving on m1 chip resolved

--- Sources/KeyboardDismissingView.swift
+++ Sources/KeyboardDismissingView.swift
@@ -9,21 +9,21 @@
 import UIKit
 
 @objc public class KeyboardDismissingView: UIView {
-    
+
     public var dismissingBlock: (() -> Void)?
     public var touchEndedBlock: (() -> Void)?
-    
+
     override public func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesEnded(touches, with: event)
-        
+
         let isDismissing = KeyboardDismissingView.resignAnyFirstResponder(self)
-        
+
         if isDismissing {
             self.dismissingBlock?()
         }
         self.touchEndedBlock?()
     }
-    
+
     @discardableResult public class func resignAnyFirstResponder(_ view: UIView) -> Bool {
         var hasResigned = false
         for subView in view.subviews {
@@ -33,8 +33,7 @@ import UIKit
                 if let searchBar = subView as? UISearchBar {
                     searchBar.setShowsCancelButton(false, animated: true)
                 }
-            }
-            else {
+            } else {
                 hasResigned = KeyboardDismissingView.resignAnyFirstResponder(subView) || hasResigned
             }
         }
