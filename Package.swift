b5441dd623eb7e59d8adb699018e6a3c81216b31
Todd Kirby <ffmpeg.php@gmail.com>
2022-09-08 23:49:22 +0700

    Update Package.swift for XCode 13

--- Package.swift
+++ Package.swift
@@ -1,5 +1,23 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "IHKeyboardAvoiding"
+    name: "IHKeyboardAvoiding",
+    platforms: [
+       .iOS(.v8)
+    ],
+    products: [
+        .library(
+            name: "IHKeyboardAvoiding",
+            targets: ["IHKeyboardAvoiding"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "IHKeyboardAvoiding",
+            dependencies: [], 
+         path: "Sources")
+    ]
 )
